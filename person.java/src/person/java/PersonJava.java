6901e8ae866aacdc0addeefe5c33c065619b6e25
dwi-agustina <dwiagustina03418@gmail.com>
2021-03-16 00:40:42 -0700

    first commit

---
+++ person.java/src/person/java/PersonJava.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package person.java;
+
+/**
+ *
+ * @author axioo
+ */
+public class PersonJava {
+
+    /** String name;
+    int age;
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO co this.name = name;
+        String fName, IName, stuStatus;
+        int stuId;
+        
+        fName = "Lisa Palombo";
+        IName = "Lisa";
+        stuStatus = "Active";
+        stuId = 123456789;
+        
+        System.out.println("Student Name : " + fName);
+        System.out.println("IName : " + IName);
+        System.out.println("Student Status : " + stuStatus);
+        System.out.println("Student ID : " + stuId);
+    }
+    
+}
